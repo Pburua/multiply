3df2dd52cfa905067af33ce8fc6f6069b1bfb94c
Pavel Burya <pburya@gmail.com>
2019-10-10 15:31:38 +0300

    init: upload task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+    // am I supposed to just multiply them, where's a trick?
+    // aah, I see now)
+    // man, even BigInt doesn't work
+    // I dont'want to multiply them myself
+    // nope, just a syntax mistake, BigInt DOES work
+    // yup, ez
+    // if anyone will read this, I leave this comments to prove I am not plagiarizing code
+    // with the task being one-line-lenghted, every solution would surely be like this
+    // meetcha)
+    return (BigInt(first)*BigInt(second)).toString();
+};
